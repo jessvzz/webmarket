473c5194c03763859150053db0da2a1da83e66a8
geaviozzi <geaviozzi@icloud.com>
2024-08-20 18:27:17 +0200

    admin dashbopard resa più dinamica

--- src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/AdminHomepage.java
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/AdminHomepage.java
@@ -2,33 +2,52 @@ package it.univaq.f4i.iw.ex.webmarket.controller;
 
 import it.univaq.f4i.iw.framework.data.DataException;
 import it.univaq.f4i.iw.ex.webmarket.data.dao.impl.ApplicationDataLayer;
+import it.univaq.f4i.iw.ex.webmarket.data.model.Utente;
 import it.univaq.f4i.iw.framework.result.TemplateManagerException;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
+import it.univaq.f4i.iw.framework.security.SecurityHelpers;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class AdminHomepage extends BaseController {
 
     private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
         TemplateResult res = new TemplateResult(getServletContext());
-        request.setAttribute("page_title", "Issues");
+        request.setAttribute("page_title", "Admin Dashboard");
         res.activate("homepageadmin.ftl.html", request, response);
     }
 
-    @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException {
-
-        try {
-            action_default(request, response);
-
-        } catch (IOException | TemplateManagerException ex) {
-            handleError(ex, request, response);
+        throws ServletException {
+    try {
+        HttpSession session = SecurityHelpers.checkSession(request);
+        if (session == null) {
+            
+            // Se la sessione non è valida, torno login
+            response.sendRedirect("login");
+            return;
         }
+        
+        // trovo user
+        int userId = (int) session.getAttribute("userid");
+        Utente u = ((ApplicationDataLayer) request.getAttribute("datalayer")).getUtenteDAO().getUtente(userId);
+        
+        if (u != null) {
+            request.setAttribute("user", u);
+        }
+
+        action_default(request, response);
+
+    } catch (IOException | TemplateManagerException | DataException ex) {
+        handleError(ex, request, response);
     }
+}
+
+
 
     /**
      * Returns a short description of the servlet.
