10f194a1fdece1e70bd4f72eecc28be523c40568
geaviozzi <geaviozzi@icloud.com>
2024-08-21 13:54:54 +0200

    iniziato template gestione utenti

---
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/GestioneUtenti.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.univaq.f4i.iw.ex.webmarket.controller;
+
+import it.univaq.f4i.iw.ex.webmarket.data.dao.impl.ApplicationDataLayer;
+import it.univaq.f4i.iw.ex.webmarket.data.model.Utente;
+import it.univaq.f4i.iw.framework.data.DataException;
+import it.univaq.f4i.iw.framework.result.TemplateManagerException;
+import it.univaq.f4i.iw.framework.result.TemplateResult;
+import it.univaq.f4i.iw.framework.security.SecurityHelpers;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author jessviozzi
+ */
+public class GestioneUtenti extends BaseController {
+
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        TemplateResult res = new TemplateResult(getServletContext());
+        request.setAttribute("page_title", "Gestione Utenti");
+        res.activate("gestione_utenti.ftl.html", request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException {
+    try {
+        HttpSession session = SecurityHelpers.checkSession(request);
+        if (session == null) {
+            
+            // Se la sessione non è valida, torno login
+            response.sendRedirect("login");
+            return;
+        }
+        
+        // trovo user
+        int userId = (int) session.getAttribute("userid");
+        Utente u = ((ApplicationDataLayer) request.getAttribute("datalayer")).getUtenteDAO().getUtente(userId);
+        
+        if (u != null) {
+            request.setAttribute("user", u);
+        }
+
+        action_default(request, response);
+
+    } catch (IOException | TemplateManagerException | DataException ex) {
+        handleError(ex, request, response);
+    }
+}
+
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Main Newspaper servlet";
+    }// </editor-fold>
+
+}
