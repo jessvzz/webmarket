c110165db6a3034af233fc5e501a495adaa523ba
geaviozzi <geaviozzi@icloud.com>
2024-08-22 09:26:10 +0200

    problema getParameter risolto

--- src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/GestioneUtenti.java
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/GestioneUtenti.java
@@ -57,6 +57,7 @@ public class GestioneUtenti extends BaseController {
     String password = request.getParameter("temp-password");
     String confirmPassword = request.getParameter("confirm-password");
     String roleParam = request.getParameter("role");
+    System.out.println("user: " + username);
     
     if (username == null || email == null || password == null || confirmPassword == null || roleParam == null) {
         request.setAttribute("error", "Tutti i campi sono obbligatori.");
