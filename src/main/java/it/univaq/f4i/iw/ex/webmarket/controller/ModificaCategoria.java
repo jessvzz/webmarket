5977d9c79fe15c965c5837cecbe8cb47190d998a
geaviozzi <geaviozzi@icloud.com>
2024-08-23 10:28:56 +0200

    iniziato modifica categoria

---
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/ModificaCategoria.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.univaq.f4i.iw.ex.webmarket.controller;
+
+import it.univaq.f4i.iw.ex.webmarket.data.dao.impl.ApplicationDataLayer;
+import it.univaq.f4i.iw.ex.webmarket.data.model.Categoria;
+import it.univaq.f4i.iw.framework.data.DataException;
+import it.univaq.f4i.iw.framework.result.SplitSlashesFmkExt;
+import it.univaq.f4i.iw.framework.result.TemplateManagerException;
+import it.univaq.f4i.iw.framework.result.TemplateResult;
+import it.univaq.f4i.iw.framework.security.SecurityHelpers;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jessviozzi
+ */
+public class ModificaCategoria extends BaseController{
+     private void action_categoria(HttpServletRequest request, HttpServletResponse response, int n) throws IOException, ServletException, TemplateManagerException, DataException {
+        try {
+            Categoria categoria = ((ApplicationDataLayer) request.getAttribute("datalayer")).getCategoriaDAO().getCategoria(n);
+            if (categoria != null) {
+                request.setAttribute("categoria", categoria);
+                
+                
+                request.setAttribute("page_title", "modifica categoria");
+                //verr� usato automaticamente il template di outline spcificato tra i context parameters
+                //the outlne template specified through the context parameters will be added by the TemplateResult to the specified template
+                TemplateResult res = new TemplateResult(getServletContext());
+                //aggiungiamo al template un wrapper che ci permette di chiamare la funzione stripSlashes
+                //add to the template a wrapper object that allows to call the stripslashes function
+                request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+                res.activate("modifica_categoria.ftl.html", request, response);
+            } else {
+                handleError("Unable to load category", request, response);
+            }
+        } catch (DataException ex) {
+            handleError("Data access exception: " + ex.getMessage(), request, response);
+        }
+    }
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+
+        int n;
+        try {
+            n = SecurityHelpers.checkNumeric(request.getParameter("n"));
+            action_categoria(request, response, n);
+        } catch (NumberFormatException ex) {
+            handleError("Invalid number specified", request, response);
+        } catch (IOException | TemplateManagerException ex) {
+            handleError(ex, request, response);
+        } catch (DataException ex) {
+             Logger.getLogger(CategoriaController.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Categoria servlet";
+    }// </editor-fold>
+}
