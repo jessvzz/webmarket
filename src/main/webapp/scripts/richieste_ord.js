edd8fa4b1e709433cda49d9393eb878d78fea60c
samanta95-oop <83903789+samanta95-oop@users.noreply.github.com>
2024-08-31 16:45:40 +0200

    js per stato richieste ordinante

---
+++ src/main/webapp/scripts/richieste_ord.js
@@ -0,0 +1,33 @@
+function cambiaStatoRichiesta(statoRichiesta) {
+    let backgroundColor;
+
+    switch (statoRichiesta) {
+        case 'IN_ATTESA':
+            backgroundColor = '#ff6347'; // red
+            break;
+        case 'PRESA_IN_CARICO':
+            backgroundColor = '#ff7f50'; // orange
+            break;
+        case 'RISOLTA':
+            backgroundColor = '#ffd700'; // yellow
+            break;
+        case 'ORDINATA':
+            backgroundColor = '#adff2f'; // green
+            break;
+        default:            
+        backgroundColor = "#FFFFFF";
+}
+return `
+    <div class="card-row-content" style="background-color: ${backgroundColor};">
+        <p class="card-row-text">${statoRichiesta}</p>
+    </div>
+`;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const richieste = document.querySelectorAll('.card-row-content[stato]');
+    richieste.forEach(richiesta => {
+        const statoRichiesta = richiesta.getAttribute('stato');
+        richiesta.outerHTML = cambiaStatoRichiesta(statoRichiesta);
+    });
+});
