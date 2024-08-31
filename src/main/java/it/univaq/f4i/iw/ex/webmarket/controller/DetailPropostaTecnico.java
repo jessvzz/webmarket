a60cae025a90589c04da74a2c9293c4fda958caf
geaviozzi <geaviozzi@icloud.com>
2024-08-31 11:11:50 +0200

    implementazione email con pdf

--- src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/DetailPropostaTecnico.java
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/DetailPropostaTecnico.java
@@ -1,5 +1,6 @@
 package it.univaq.f4i.iw.ex.webmarket.controller;
 
+import com.itextpdf.text.DocumentException;
 import it.univaq.f4i.iw.ex.webmarket.data.dao.impl.ApplicationDataLayer;
 import it.univaq.f4i.iw.ex.webmarket.data.model.Ordine;
 import it.univaq.f4i.iw.ex.webmarket.data.model.PropostaAcquisto;
@@ -14,8 +15,10 @@ import it.univaq.f4i.iw.framework.result.TemplateManagerException;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
 import it.univaq.f4i.iw.framework.security.SecurityHelpers;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.mail.Session;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -41,12 +44,39 @@ public class DetailPropostaTecnico extends BaseController {
         PropostaAcquisto proposta = ((ApplicationDataLayer) request.getAttribute("datalayer")).getPropostaAcquistoDAO().getPropostaAcquisto(n);
         proposta.setStatoProposta(StatoProposta.ORDINATO);
         ((ApplicationDataLayer) request.getAttribute("datalayer")).getPropostaAcquistoDAO().storePropostaAcquisto(proposta);
+        String email = proposta.getRichiestaOrdine().getUtente().getEmail();
 
         Ordine ordine = new OrdineImpl();
         ordine.setProposta(proposta);
         ordine.setStato(StatoOrdine.IN_ATTESA);
         ((ApplicationDataLayer) request.getAttribute("datalayer")).getOrdineDAO().storeOrdine(ordine);
         
+        //gestione email
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.outlook.com"); 
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new javax.mail.PasswordAuthentication("webmarket.univaq@outlook.com", "geagiuliasamanta1");
+            }
+        });
+        String text = "Gentile Utente, la informiamo che è stato effettuato un ordine per la sua proposta numero " + proposta.getCodice() +"\n\n In allegato trova i dettagli del suo ordine.";
+        // genero PDF
+        String messaggio = "\n Dettagli dell'ordine effettuato per la proposta numero: "+ proposta.getCodice()+"\n\n";
+        String pdfFilePath = "OrdineProposta_" + proposta.getCodice() + ".pdf";
+
+        try {
+            EmailSender.createPDF(messaggio, proposta);
+
+           
+            EmailSender.sendEmailWithAttachment(session, email, "Notifica Ordine", text, pdfFilePath);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
         response.sendRedirect("storico_tecnico"); 
         
 
--- src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/EmailSender.java
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/EmailSender.java
@@ -4,15 +4,34 @@
  */
 package it.univaq.f4i.iw.ex.webmarket.controller;
 
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import it.univaq.f4i.iw.ex.webmarket.data.model.PropostaAcquisto;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
 
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
 
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
 
 public class EmailSender {
 
@@ -53,4 +72,83 @@ public class EmailSender {
 	      e.printStackTrace();
 	    }
 	}
+        
+        public static void createPDF(String messaggio, PropostaAcquisto proposta) throws FileNotFoundException, DocumentException{
+           Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("OrdineProposta_" + proposta.getCodice() + ".pdf"));
+            document.open();
+
+            Font font = FontFactory.getFont(FontFactory.TIMES, 14, BaseColor.BLACK);
+            Font bold = FontFactory.getFont(FontFactory.TIMES_BOLD, 14, BaseColor.BLACK);
+
+
+            Chunk greetingChunk = new Chunk(messaggio, font);
+            document.add(greetingChunk);
+
+            Paragraph details = new Paragraph();
+            details.add(new Chunk("Codice Proposta: ", bold));
+            details.add(new Chunk(proposta.getCodice() + "\n", font));
+            details.add(new Chunk("Produttore: ", bold));
+            details.add(new Chunk(proposta.getProduttore() + "\n", font));
+            details.add(new Chunk("Prodotto: ", bold));
+            details.add(new Chunk(proposta.getProdotto() + "\n", font));
+            details.add(new Chunk("Codice Prodotto: ", bold));
+            details.add(new Chunk(proposta.getCodiceProdotto() + "\n", font));
+            details.add(new Chunk("Prezzo: ", bold));
+            details.add(new Chunk(proposta.getPrezzo() + "\n", font));
+            details.add(new Chunk("URL: ", bold));
+            details.add(new Chunk(proposta.getUrl() + "\n", font));
+
+            document.add(details);
+            
+            document.close();
+            System.out.println("PDF generato con successo!");
+
+        } catch (DocumentException | IOException e) {
+            e.printStackTrace();
+        }
+        }
+        
+        public static void sendEmailWithAttachment(Session session, String toEmail, String subject, String body, String pdfFilePath) {
+        try {
+            MimeMessage msg = new MimeMessage(session);
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("webmarket.univaq@outlook.com", "WebMarket"));
+
+            msg.setReplyTo(InternetAddress.parse("webmarket.univaq@outlook.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText(body, "UTF-8");
+
+            // Creazione della parte allegato
+            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+            attachmentBodyPart.attachFile(new File(pdfFilePath));
+            attachmentBodyPart.setFileName(MimeUtility.encodeText(new File(pdfFilePath).getName(), "UTF-8", null));
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            multipart.addBodyPart(attachmentBodyPart);
+
+            msg.setContent(multipart);
+
+            // Invio dell'email
+            Transport.send(msg);
+
+            System.out.println("Email con allegato PDF inviata con successo!");
+
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
