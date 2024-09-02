e744a9aaa60cdfb705b367099b3a70dd1deba3e6
geaviozzi <geaviozzi@icloud.com>
2024-09-02 12:23:58 +0200

    iniziato dettaglio ordine tecnico ma devo cambiarlo completamente

--- src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/DetailStoricoTecnico.java
+++ src/main/java/it/univaq/f4i/iw/ex/webmarket/controller/DetailStoricoTecnico.java
@@ -1,6 +1,7 @@
 package it.univaq.f4i.iw.ex.webmarket.controller;
 
 import it.univaq.f4i.iw.ex.webmarket.data.dao.impl.ApplicationDataLayer;
+import it.univaq.f4i.iw.ex.webmarket.data.model.Ordine;
 import it.univaq.f4i.iw.ex.webmarket.data.model.Utente;
 import it.univaq.f4i.iw.ex.webmarket.data.model.impl.TipologiaUtente;
 import it.univaq.f4i.iw.ex.webmarket.data.model.impl.UtenteImpl;
@@ -9,6 +10,8 @@ import it.univaq.f4i.iw.framework.result.TemplateManagerException;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
 import it.univaq.f4i.iw.framework.security.SecurityHelpers;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,16 +19,23 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Giulia Di Flamminio
+ * @author Gea Viozzi
  */
 public class DetailStoricoTecnico extends BaseController {
 
-    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+    private void action_default(HttpServletRequest request, HttpServletResponse response, int n) throws IOException, ServletException, TemplateManagerException, DataException {
         TemplateResult res = new TemplateResult(getServletContext());
         request.setAttribute("page_title", "Detail storico");
+        
+        Ordine ordine = ((ApplicationDataLayer) request.getAttribute("datalayer")).getOrdineDAO().getOrdine(n);
+        System.out.println("proposta: " + ordine.getProposta().getCodice());
+        
+        request.setAttribute("ordine", ordine);
+        
         res.activate("detailstorico_tecnico.ftl.html", request, response);
     }
 
+    @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
         throws ServletException {
     try {
@@ -34,13 +44,17 @@ public class DetailStoricoTecnico extends BaseController {
             response.sendRedirect("login");
             return;
         }
-
+        int n;
+        n = SecurityHelpers.checkNumeric(request.getParameter("n"));
+        
         String action = request.getParameter("action");
-        action_default(request, response);
+        action_default(request, response, n);
 
     } catch (IOException | TemplateManagerException /* | DataException */ ex) {
         handleError(ex, request, response);
-    }
+    }   catch (DataException ex) {
+            Logger.getLogger(DetailStoricoTecnico.class.getName()).log(Level.SEVERE, null, ex);
+        }
 }
 
 
